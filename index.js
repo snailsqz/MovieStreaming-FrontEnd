c64850a6153018807ed5d964671ca5922a990688
snails <snailsqz@gmail.com>
2024-02-23 19:23:47 +0700

    save

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 
 const app = express();
-const base_url = "http://localhost:3000";
+const base_url = "http://node57177-pawee-noderest.proen.app.ruk-com.cloud";
 app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
