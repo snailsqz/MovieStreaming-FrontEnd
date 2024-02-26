be6909184573f32d2b0f36519eb205eeebc0aebc
snails <snailsqz@gmail.com>
2024-02-27 01:03:22 +0700

    Update base URL to use HTTPS

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 const session = require("express-session");
 
-const base_url = "http://localhost:3000";
+const base_url = "https://moviestream-backend.onrender.com";
 
 app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
