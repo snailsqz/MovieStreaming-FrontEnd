47b1e99f4c9c258afe874fe8afd23ce8013f55e0
snails <snailsqz@gmail.com>
2024-03-02 02:55:25 +0700

    Update base URL to production environment

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 const session = require("express-session");
 
-const base_url = "http://localhost:3000";
+const base_url =
+  "http://node57177-pawee-noderest.proen.app.ruk-com.cloud:11397";
 
 app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
